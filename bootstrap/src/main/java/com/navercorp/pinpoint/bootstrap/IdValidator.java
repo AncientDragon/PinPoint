f404d3913c9f2403a32e38a182b0dac2665d71ac
kulingkwi <kulingkwi@gmail.com>
2017-06-20 18:07:23 +0800

    modify the max length of agent name and application name

--- bootstrap/src/main/java/com/navercorp/pinpoint/bootstrap/IdValidator.java
+++ bootstrap/src/main/java/com/navercorp/pinpoint/bootstrap/IdValidator.java
@@ -30,7 +30,7 @@ public class IdValidator {
     private final BootLogger logger = BootLogger.getLogger(IdValidator.class.getName());
 
     private final Properties property;
-    private static final int MAX_ID_LENGTH = 24;
+    private static final int MAX_ID_LENGTH = 50;
 
     public IdValidator() {
         this(System.getProperties());
